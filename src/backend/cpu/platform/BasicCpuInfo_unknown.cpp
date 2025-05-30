a003f9dd1234822b639184c3a121378e7752076a
WHR <msl0000023508@gmail.com>
2025-05-30 15:19:20 +0800

    Correct arguments of function 'xmrig::BasicCpuInfo::threads' in src/backend/cpu/platform/BasicCpuInfo_unknown.cpp

--- src/backend/cpu/platform/BasicCpuInfo_unknown.cpp
+++ src/backend/cpu/platform/BasicCpuInfo_unknown.cpp
@@ -40,6 +40,6 @@ const char *xmrig::BasicCpuInfo::backend() const {
 }
 
 
-xmrig::CpuThreads xmrig::BasicCpuInfo::threads(const Algorithm &) const {
+xmrig::CpuThreads xmrig::BasicCpuInfo::threads(const Algorithm &, uint32_t) const {
 	return CpuThreads(threads());
 }
